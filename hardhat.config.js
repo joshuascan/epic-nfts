94638b7bd2af05f85db3e43820f9420f95716fa3
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-03-28 17:46:56 -0700

    adds etherscan verification

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
 module.exports = {
   solidity: "0.8.1",
@@ -8,5 +9,8 @@ module.exports = {
       url: process.env.ALCHEMY_API_URL,
       accounts: [process.env.PRIVATE_KEY],
     },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY,
   },
 };
